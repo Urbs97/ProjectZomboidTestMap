b9e11ef6d76c71ec75be4ff11b8943319201e74d
Urbs97 <urbs97@pm.me>
2024-03-09 18:59:33 +0100

    Initial Testproject

---
+++ MyMapMod/media/maps/objects.lua
@@ -0,0 +1,13 @@
+objects = {
+  { name = "", type = "Forest", x = 272, y = 321, z = 0, width = 28, height = 31 },
+  { name = "", type = "SpawnPoint", x = 275, y = 310, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "constructionworker,fireofficer,parkranger,policeofficer,securityguard,unemployed"
+    }
+  },
+  { name = "", type = "SpawnPoint", x = 279, y = 317, z = 0, width = 1, height = 1,
+    properties = {
+      Professions = "constructionworker,fireofficer,parkranger,policeofficer,securityguard,unemployed"
+    }
+  }
+}
---
+++ MyMapMod/media/maps/spawnpoints.lua
@@ -0,0 +1,28 @@
+function SpawnPoints()
+return {
+  constructionworker = {
+    { worldX = 0, worldY = 1, posX = 275, posY = 10, posZ = 0 },
+    { worldX = 0, worldY = 1, posX = 279, posY = 17, posZ = 0 }
+  },
+  fireofficer = {
+    { worldX = 0, worldY = 1, posX = 275, posY = 10, posZ = 0 },
+    { worldX = 0, worldY = 1, posX = 279, posY = 17, posZ = 0 }
+  },
+  parkranger = {
+    { worldX = 0, worldY = 1, posX = 275, posY = 10, posZ = 0 },
+    { worldX = 0, worldY = 1, posX = 279, posY = 17, posZ = 0 }
+  },
+  policeofficer = {
+    { worldX = 0, worldY = 1, posX = 275, posY = 10, posZ = 0 },
+    { worldX = 0, worldY = 1, posX = 279, posY = 17, posZ = 0 }
+  },
+  securityguard = {
+    { worldX = 0, worldY = 1, posX = 275, posY = 10, posZ = 0 },
+    { worldX = 0, worldY = 1, posX = 279, posY = 17, posZ = 0 }
+  },
+  unemployed = {
+    { worldX = 0, worldY = 1, posX = 275, posY = 10, posZ = 0 },
+    { worldX = 0, worldY = 1, posX = 279, posY = 17, posZ = 0 }
+  }
+}
+end
